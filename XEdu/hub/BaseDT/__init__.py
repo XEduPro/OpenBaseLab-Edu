0db401223e02fcd9355fc3329fd9e5a6e4d81432
linmy <657894692@qq.com>
2026-03-05 15:49:42 +0800

    refactor: BaseDT 增加 npz 简短入口，示例改为 npz_demo 并精简

--- XEdu/hub/BaseDT/__init__.py
+++ XEdu/hub/BaseDT/__init__.py
@@ -2,5 +2,6 @@
 """BaseDT 数据工具模块。"""
 
 from .dataset import make_npz_dataset, NPZGenerator
+from . import npz
 
-__all__ = ['make_npz_dataset', 'NPZGenerator']
+__all__ = ['make_npz_dataset', 'NPZGenerator', 'npz']
